769bb4c0c940f1c5b330909646a969b8926b758e
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-11-12 12:02:43 +0100

    Composite class holding pointers to other objects

---
+++ gl_05/Composite.cpp
@@ -0,0 +1,30 @@
+#include "Composite.h"
+
+Composite::Composite()
+{
+}
+
+void Composite::move(const glm::vec3& displacement)
+{
+}
+
+void Composite::rotate(const glm::vec3& angle)
+{
+}
+
+void Composite::scale(const glm::vec3& factor)
+{
+}
+
+void Composite::addElement()
+{
+}
+
+void Composite::removeElement(int index)
+{
+}
+
+std::unique_ptr<Object>& Composite::getElement(int index)
+{
+	return elements.at(index);
+}
---
+++ gl_05/Composite.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Object.h"
+#include "Shape.h"
+#include <memory>
+
+class Composite : public Object
+{
+protected:
+	std::vector<std::unique_ptr<Object>> elements;
+public:
+	Composite();
+	~Composite() = default;
+	void draw();
+	void move(const glm::vec3& displacement) override;
+	void rotate(const glm::vec3& angle) override;
+	void scale(const glm::vec3& factor) override;
+	void addElement();
+	void removeElement(int index);
+	std::unique_ptr<Object>& getElement(int index);
+
+};
+

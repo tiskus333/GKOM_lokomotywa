4a422ba2c17756113b2d736d038eed6a82dc752c
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-11-12 12:01:23 +0100

    added base class to inherit from

---
+++ gl_05/Object.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <glm/glm.hpp>
+
+class Object
+{
+protected:
+	glm::vec3 position;
+	Object* parent;
+
+public:
+	virtual void draw() = 0;
+	virtual void move(const glm::vec3& displacement) = 0;
+	virtual void rotate(const glm::vec3& angle) = 0;
+	virtual void scale(const glm::vec3& factor) = 0;
+	virtual ~Object() = default;
+
+};
+

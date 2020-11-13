98906117394dd69e1433345d8f0b136191ef6fc6
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-11-13 21:48:01 +0100

    removed functions not common for all objects

--- files/Object.h
+++ files/Object.h
@@ -9,11 +9,8 @@ protected:
 	glm::vec3 rotation_;
 
 public:
-	virtual void draw() = 0;
 	virtual void move(const glm::vec3& displacement) = 0;
 	virtual void rotate(const glm::vec3& angle, const glm::vec3& point) = 0;
-	virtual void scale(const glm::vec3& factor) = 0;
-	virtual ~Object() = 0;
 
 };
 

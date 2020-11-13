d4da9c6d57ed3c3c1c1cd7839784ab93f1222bfe
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-11-13 21:10:20 +0100

    rotation is now a basic parameter for all objects

--- files/Object.h
+++ files/Object.h
@@ -6,13 +6,14 @@ class Object
 protected:
 	glm::vec3 position_;
 	Object* parent_;
+	glm::vec3 rotation_;
 
 public:
 	virtual void draw() = 0;
 	virtual void move(const glm::vec3& displacement) = 0;
-	virtual void rotate(const glm::vec3& angle) = 0;
+	virtual void rotate(const glm::vec3& angle, const glm::vec3& point) = 0;
 	virtual void scale(const glm::vec3& factor) = 0;
-	virtual ~Object() = default;
+	virtual ~Object() = 0;
 
 };
 

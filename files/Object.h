813d47659fc734baac33a02c45768c117233479b
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-11-18 22:39:32 +0100

    removed parent

--- files/Object.h
+++ files/Object.h
@@ -3,12 +3,10 @@
 
 class Object
 {
-protected:
+public:
 	glm::vec3 position_;
-	Object* parent_;
 	glm::vec3 rotation_;
 
-public:
 	virtual void move(const glm::vec3& displacement) = 0;
 	virtual void rotate(const glm::vec3& angle, const glm::vec3& point) = 0;
 

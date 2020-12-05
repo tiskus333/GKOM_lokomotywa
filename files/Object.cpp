90de0e3eef9e41dbd31713bd68eca4f06ae9c9d2
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-12-05 02:22:46 +0100

    rearenged rotations

--- files/Object.cpp
+++ files/Object.cpp
@@ -9,9 +9,9 @@ void Object::rotate(const glm::vec3& angle, const glm::vec3& point)
 {
 	glPushMatrix();
 	rotation_matrix_ = glm::translate(rotation_matrix_, point);
-	rotation_matrix_ = glm::rotate(rotation_matrix_, -glm::radians(angle.y), glm::vec3(0.0f, 1.0f, 0.0f));
-	rotation_matrix_ = glm::rotate(rotation_matrix_, -glm::radians(angle.z), glm::vec3(0.0f, 0.0f, 1.0f));
-	rotation_matrix_ = glm::rotate(rotation_matrix_, -glm::radians(angle.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	rotation_matrix_ = glm::rotate(rotation_matrix_, glm::radians(angle.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	rotation_matrix_ = glm::rotate(rotation_matrix_, glm::radians(angle.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	rotation_matrix_ = glm::rotate(rotation_matrix_, glm::radians(angle.z), glm::vec3(0.0f, 0.0f, 1.0f));
 	rotation_matrix_ = translate(rotation_matrix_, -point);
 	glPopMatrix();
 }

64458453d6a48ac241fa50d77456c067945b5112
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-12-05 13:07:54 +0100

    rotation now doesnt change axis

--- files/Object.cpp
+++ files/Object.cpp
@@ -8,17 +8,29 @@ void Object::move(const glm::vec3& displacement)
 void Object::rotate(const glm::vec3& angle, const glm::vec3& point)
 {
 	glPushMatrix();
-	rotation_matrix_ = glm::translate(rotation_matrix_, point);
-	rotation_matrix_ = glm::rotate(rotation_matrix_, glm::radians(angle.x), glm::vec3(1.0f, 0.0f, 0.0f));
-	rotation_matrix_ = glm::rotate(rotation_matrix_, glm::radians(angle.y), glm::vec3(0.0f, 1.0f, 0.0f));
-	rotation_matrix_ = glm::rotate(rotation_matrix_, glm::radians(angle.z), glm::vec3(0.0f, 0.0f, 1.0f));
-	rotation_matrix_ = translate(rotation_matrix_, -point);
+	glm::mat4 rotation;
+	rotation = glm::translate(rotation, point);
+	rotation = glm::rotate(rotation, glm::radians(angle.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	rotation = glm::rotate(rotation, glm::radians(angle.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	rotation = glm::rotate(rotation, glm::radians(angle.z), glm::vec3(0.0f, 0.0f, 1.0f));
+	rotation = translate(rotation, -point);
+	rotation_matrix_ = rotation * rotation_matrix_;
 	glPopMatrix();
 }
 
 void Object::rotate(const glm::vec3& angle)
 {
-	this->rotate(angle, position_);
+	/*glPushMatrix();
+	glm::mat4 rotation;
+	rotation = glm::rotate(rotation, glm::radians(angle.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	rotation = glm::rotate(rotation, glm::radians(angle.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	rotation = glm::rotate(rotation, glm::radians(angle.z), glm::vec3(0.0f, 0.0f, 1.0f));
+	rotation_matrix_ = rotation_matrix_* rotation;
+	glPopMatrix();*/
+	if (parent_ != nullptr)
+		this->rotate(angle, parent_->position_ + position_ );
+	else
+		this->rotate(angle, position_);
 }
 
 void Object::setPosition(const glm::vec3& position)

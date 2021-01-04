5e01b1e314273ef911033984a05b5a382fd7aade
pktiuk <kotiuk@wp.pl>
2021-01-04 20:20:14 +0100

    Add base methods for managing light surces

--- files/Scene.cpp
+++ files/Scene.cpp
@@ -1,10 +1,14 @@
 
 #include "Scene.h"
 
-Scene::Scene(/* args */) : ambient_light(0.2, 0.2, 0.2)
+Scene::Scene(/* args */) : number_of_lights(0), ambient_light(0.2, 0.2, 0.2)
 {
     shape_shader = ShaderProgram("CubeShader.vert", "CubeShader.frag");
     light_shader = ShaderProgram("LightShader.vert", "LightShader.frag");
+    for (int i = 0; i < MAX_NUM_OF_POINT_LIGHTS; i++)
+    {
+        is_light_used[i] = false;
+    }
 }
 
 Scene::~Scene()
@@ -20,13 +24,27 @@ Scene &Scene::getScene()
 void Scene::setVec3InShaders(const std::string &name, glm::vec3 &value)
 {
     shape_shader.Use();
-    shape_shader.setVec3(name,value);
-    light_shader.setVec3(name,value);
+    shape_shader.setVec3(name, value);
+    light_shader.setVec3(name, value);
 }
-
 
 void Scene::setMatrix4fvInShaders(const std::string &name, glm::mat4 &matrix)
 {
-    shape_shader.setMatrix4fv(name,matrix);
-    light_shader.setMatrix4fv(name,matrix);
+    shape_shader.setMatrix4fv(name, matrix);
+    light_shader.setMatrix4fv(name, matrix);
+}
+
+void Scene::addPointLightSource(glm::vec3 lightPos, glm::vec3 lightColor)
+{
+
+}
+
+void Scene::updatePointLightSource(unsigned int number, glm::vec3 lightPos, glm::vec3 lightColor)
+{
+
+}
+
+void Scene::removePointLightSource(unsigned int number)
+{
+
 }
--- files/Scene.h
+++ files/Scene.h
@@ -1,15 +1,23 @@
 #pragma once
 
+#include <vector>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <GL/glew.h>
 #include "shprogram.h"
+
+#define MAX_NUM_OF_POINT_LIGHTS 10
 
 class Scene
 {
 private:
     Scene(/* args */);
     ~Scene();
+
+    glm::vec3 light_positions[MAX_NUM_OF_POINT_LIGHTS];
+    glm::vec3 light_colors[MAX_NUM_OF_POINT_LIGHTS];
+    bool is_light_used[MAX_NUM_OF_POINT_LIGHTS];
+    unsigned int number_of_lights;
 
 public:
     glm::vec3 ambient_light;
@@ -23,4 +31,9 @@ public:
 
     void setVec3InShaders(const std::string &name, glm::vec3 &value);
     void setMatrix4fvInShaders(const std::string &name, glm::mat4 &matrix);
+
+    void addPointLightSource(glm::vec3 lightPos,glm::vec3 lightColor);
+    void updatePointLightSource(unsigned int number, glm::vec3 lightPos,glm::vec3 lightColor);
+    void removePointLightSource(unsigned int number);
+    unsigned int getNUmberOfLights() { return number_of_lights; }
 };

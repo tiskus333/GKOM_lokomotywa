05f7ea2bf1a8209849fa03b4f0c3439dd0879711
michal <mi.sob1238@gmail.com>
2021-01-11 16:35:10 +0100

    improvements to the model

--- files/Locomotive.cpp
+++ files/Locomotive.cpp
@@ -8,6 +8,7 @@ Locomotive::Locomotive()
 	wheelVector.push_back(&BackWheels1);
 	DriverBoothRoof.rotate({ 90,0,0 });
 	LocomotiveHullRoof.rotate({ 90,0,0 });
+	LocomotiveHeadlight.rotate({ 90,0,0 });
 
 	Loc.addElement(DriverBoothBody);
 	Loc.addElement(DriverBoothRoof);
@@ -18,11 +19,18 @@ Locomotive::Locomotive()
 	Loc.addElement(Chimney1);
 	Loc.addElement(ChimneyTop1);
 	Loc.addElement(LocomotiveSuspension);
+	Loc.addElement(LocomotiveHeadlight);
+	//Loc.addElement(Bail);
+	//Loc.addElement(Bail1);
+	FrontWheels.addElement(Bail3);
 }
 
 void Locomotive::draw()
 {
 	Loc.draw();
+	Bail.draw();
+	Bail1.draw();
+	//Bail3.draw();
 	for (auto w : wheelVector)
 		w->draw();
 }
@@ -32,8 +40,11 @@ void Locomotive::move(const glm::vec3 moveVector)
 	float angle = moveVector.z * 360.0 / (2 * 3.1415 * 0.21);
 	for (auto w : wheelVector)
 	{
-		w->rotate({ angle,0,0 });
+		//w->rotate({ angle,0,0 });
 		w->move(moveVector);
+		
 	}
+	Bail3.rotate({ -angle,0,0 });
+	Bail3.draw();
 	Loc.move(moveVector);
 }
--- files/Locomotive.h
+++ files/Locomotive.h
@@ -19,6 +19,10 @@ class Locomotive
 	WagonWheels BackWheels = WagonWheels({ 0, -0.71, -0.55 });
 	WagonWheels BackWheels1 = WagonWheels({ 0, -0.71, -1.0 });
 	Cuboid LocomotiveSuspension = Cuboid({ 0,-0.6,0 }, { 0.75,0.2,2.4 }, glm::vec3(0, 2, 1));
+	Cylinder LocomotiveHeadlight = Cylinder({ 0,-0.2,-1.3 }, { 0.3,0.1,0.3 }, glm::vec3( 10,10,1 ), true);
+	Cuboid Bail = Cuboid({0.51,-0.58,-0.0}, {0.02,0.07,2.1}, glm::vec3(1,1,1));
+	Cuboid Bail1 = Cuboid({ -0.51,-0.58,-0.0 }, { 0.02,0.07,2.1 }, glm::vec3(1, 1, 1));
+	Cuboid Bail3 = Cuboid({ 0.51,-0.58+0.71,-0.0-1 }, { 0.02,0.07,2.1 }, glm::vec3(1, 1, 1));
 	Composite Loc = Composite({ 0,0,0 });
 public:
 	Locomotive();

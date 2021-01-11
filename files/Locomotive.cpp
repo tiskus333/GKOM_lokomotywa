bcc6974332b494a7361d39cd6edc7597a010262f
michal <mi.sob1238@gmail.com>
2021-01-11 17:26:48 +0100

    Finished upgrading the model

--- files/Locomotive.cpp
+++ files/Locomotive.cpp
@@ -2,9 +2,9 @@
 
 Locomotive::Locomotive()
 {
-	wheelVector.push_back(&FrontWheels);
+	wheelVector.push_back(&FrontWheels2);
 	wheelVector.push_back(&FrontWheels1);
-	wheelVector.push_back(&BackWheels);
+	wheelVector.push_back(&BackWheels2);
 	wheelVector.push_back(&BackWheels1);
 	DriverBoothRoof.rotate({ 90,0,0 });
 	LocomotiveHullRoof.rotate({ 90,0,0 });
@@ -14,23 +14,21 @@ Locomotive::Locomotive()
 	Loc.addElement(DriverBoothRoof);
 	Loc.addElement(LocomotiveHull);
 	Loc.addElement(LocomotiveHullRoof);
-	Loc.addElement(Chimney);
-	Loc.addElement(ChimneyTop);
 	Loc.addElement(Chimney1);
 	Loc.addElement(ChimneyTop1);
+	Loc.addElement(Chimney2);
+	Loc.addElement(ChimneyTop2);
 	Loc.addElement(LocomotiveSuspension);
 	Loc.addElement(LocomotiveHeadlight);
-	//Loc.addElement(Bail);
-	//Loc.addElement(Bail1);
-	FrontWheels.addElement(Bail3);
+		
+	FrontWheels1.addElement(BailRight);
+	FrontWheels1.addElement(BailLeft);
 }
 
 void Locomotive::draw()
 {
 	Loc.draw();
-	Bail.draw();
-	Bail1.draw();
-	//Bail3.draw();
+
 	for (auto w : wheelVector)
 		w->draw();
 }
@@ -40,11 +38,13 @@ void Locomotive::move(const glm::vec3 moveVector)
 	float angle = moveVector.z * 360.0 / (2 * 3.1415 * 0.21);
 	for (auto w : wheelVector)
 	{
-		//w->rotate({ angle,0,0 });
+		w->rotate({ angle,0,0 });
 		w->move(moveVector);
 		
 	}
-	Bail3.rotate({ -angle,0,0 });
-	Bail3.draw();
+
+	BailRight.rotate({ -angle,0,0 }, { 0.51,0.13,-0.05 });
+	BailLeft.rotate({ -angle,0,0 }, { -0.51,0.13,-0.05 });
+	
 	Loc.move(moveVector);
 }
--- files/Locomotive.h
+++ files/Locomotive.h
@@ -10,19 +10,19 @@ class Locomotive
 	Cuboid DriverBoothBody = Cuboid({ 0,0,0.85 }, { 1,1,0.8 }, glm::vec3(1, 0.5, 1));
 	Cylinder LocomotiveHullRoof = Cylinder({ 0,-0.1,-0.35 }, { 1,1.799, 0.4 }, glm::vec3(0, 0, 1));
 	Cuboid LocomotiveHull = Cuboid({ 0,-0.3,-0.35 }, { 1,0.4,1.8 }, glm::vec3(1, 0.5, 1));
-	Cuboid Chimney = Cuboid({ 0,0.2,-0.4 }, { 0.2,0.6,0.2 }, glm::vec3(0, 0, 0));
-	Cuboid ChimneyTop = Cuboid({ 0,0.5,-0.4 }, { 0.3,0.1,0.3 }, glm::vec3(0, 0, 0));
-	Cuboid Chimney1 = Cuboid({ 0,0.2,-1.0 }, { 0.2,0.6,0.2 }, glm::vec3(0, 0, 0));
-	Cuboid ChimneyTop1 = Cuboid({ 0,0.5,-1.0 }, { 0.3,0.1,0.3 }, glm::vec3(0, 0, 0));
-	WagonWheels FrontWheels = WagonWheels({ 0,-0.71,1.0 });
-	WagonWheels FrontWheels1 = WagonWheels({ 0, -0.71, 0.55 });
-	WagonWheels BackWheels = WagonWheels({ 0, -0.71, -0.55 });
-	WagonWheels BackWheels1 = WagonWheels({ 0, -0.71, -1.0 });
+	Cuboid Chimney1 = Cuboid({ 0,0.2,-0.4 }, { 0.2,0.6,0.2 }, glm::vec3(0, 0, 0));
+	Cuboid ChimneyTop1 = Cuboid({ 0,0.5,-0.4 }, { 0.3,0.1,0.3 }, glm::vec3(0, 0, 0));
+	Cuboid Chimney2 = Cuboid({ 0,0.2,-1.0 }, { 0.2,0.6,0.2 }, glm::vec3(0, 0, 0));
+	Cuboid ChimneyTop2 = Cuboid({ 0,0.5,-1.0 }, { 0.3,0.1,0.3 }, glm::vec3(0, 0, 0));
+	WagonWheels FrontWheels1 = WagonWheels({ 0,-0.71,1.0 });
+	WagonWheels FrontWheels2 = WagonWheels({ 0, -0.71, 0.55 });
+	WagonWheels BackWheels1 = WagonWheels({ 0, -0.71, -0.55 });
+	WagonWheels BackWheels2 = WagonWheels({ 0, -0.71, -1.0 });
 	Cuboid LocomotiveSuspension = Cuboid({ 0,-0.6,0 }, { 0.75,0.2,2.4 }, glm::vec3(0, 2, 1));
 	Cylinder LocomotiveHeadlight = Cylinder({ 0,-0.2,-1.3 }, { 0.3,0.1,0.3 }, glm::vec3( 10,10,1 ), true);
-	Cuboid Bail = Cuboid({0.51,-0.58,-0.0}, {0.02,0.07,2.1}, glm::vec3(1,1,1));
-	Cuboid Bail1 = Cuboid({ -0.51,-0.58,-0.0 }, { 0.02,0.07,2.1 }, glm::vec3(1, 1, 1));
-	Cuboid Bail3 = Cuboid({ 0.51,-0.58+0.71,-0.0-1 }, { 0.02,0.07,2.1 }, glm::vec3(1, 1, 1));
+	Cuboid BailRight = Cuboid({ 0.51,0.13,- 1 }, { 0.02,0.07,2.1 }, glm::vec3(1, 1, 1));
+	Cuboid BailLeft = Cuboid({ -0.51,0.13,- 1 }, { 0.02,0.07,2.1 }, glm::vec3(1, 1, 1));
+	
 	Composite Loc = Composite({ 0,0,0 });
 public:
 	Locomotive();

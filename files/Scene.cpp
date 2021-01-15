60d601bd514a28a8b0d24b0d0cd7513b9665f2df
Maciej Kowalski <01143580@pw.edu.pl>
2021-01-15 20:48:16 +0100

    Added skybox shader

--- files/Scene.cpp
+++ files/Scene.cpp
@@ -5,6 +5,7 @@ Scene::Scene(/* args */) : number_of_lights(0), ambient_light(0.2, 0.2, 0.2)
 {
     shape_shader = ShaderProgram("CubeShader.vert", "CubeShader.frag");
     light_shader = ShaderProgram("LightShader.vert", "LightShader.frag");
+    skybox_shader = ShaderProgram("SkyBox.vert", "SkyBox.frag");
     for (int i = 0; i < MAX_NUM_OF_POINT_LIGHTS; i++)
     {
         is_light_used[i] = false;
@@ -26,12 +27,14 @@ void Scene::setVec3InShaders(const std::string &name, glm::vec3 &value)
     shape_shader.Use();
     shape_shader.setVec3(name, value);
     light_shader.setVec3(name, value);
+    skybox_shader.setVec3(name, value);
 }
 
 void Scene::setMatrix4fvInShaders(const std::string &name, glm::mat4 &matrix)
 {
     shape_shader.setMatrix4fv(name, matrix);
     light_shader.setMatrix4fv(name, matrix);
+    skybox_shader.setMatrix4fv(name, matrix);
 }
 
 unsigned int Scene::addPointLightSource(glm::vec3 lightPos, glm::vec3 lightColor)

5340476390f2a5ebb096cb6e3053620523c9d0c3
Mateusz Chrusciel <tiskus.3009@gmail.com>
2021-01-16 20:38:09 +0100

    lowered speed

--- files/Camera.h
+++ files/Camera.h
@@ -18,7 +18,7 @@ enum Camera_Movement {
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 8.0f;
+const float SPEED = 4.0f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 60.0f;
 

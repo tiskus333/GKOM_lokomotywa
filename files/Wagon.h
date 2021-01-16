bc7c3b9b91038e075d8112f4e768ebcb79db42cc
Maciej Kowalski <01143580@pw.edu.pl>
2021-01-16 19:30:16 +0100

    applied new textures

--- files/Wagon.h
+++ files/Wagon.h
@@ -10,8 +10,8 @@ class Wagon
 	WagonWheels FrontWheels1 = WagonWheels({ 0, -0.71, 0.65 });
 	WagonWheels BackWheels = WagonWheels({ 0, -0.71, -0.65 });
 	WagonWheels BackWheels1 = WagonWheels({ 0, -0.71, -1.1 });
-	Cylinder WagonRoof = Cylinder({ 0,0.5,0 }, { 1,2.999,0.5 }, glm::vec3(0.294, 0.294, 0.294),false);
-	Cuboid WagonBody = Cuboid({ 0,0,0 }, { 1,1,3 }, glm::vec3(0.294, 0.294, 0.294));
+	Cylinder WagonRoof = Cylinder({ 0,0.5,0 }, { 1,2.999,0.5 }, glm::vec3(0.874, 0.823, 0.862),false);
+	Cuboid WagonBody = Cuboid({ 0,0,0 }, { 1,1,3 }, "wagon.png");
 	Cuboid WagonSuspension = Cuboid({ 0,-0.6,0 }, { 0.75,0.2,2.8 }, glm::vec3(0.294, 0.294, 0.294),false);
 	Composite Compartment = Composite({ 0,0,0 });
 public:

c1f334ca66ee2b669c46d66d9fa4178397034048
michal <mi.sob1238@gmail.com>
2021-01-27 00:20:30 +0100

    added class responsible for one type of cacti

---
+++ files/Cacti.cpp
@@ -0,0 +1,14 @@
+#include "Cacti.h"
+Cacti::Cacti(const glm::vec3& position):Composite(position) {
+
+	
+	this->addElement(cactiBase);
+	this->addElement(cactiBranchRight);
+	this->addElement(cactiBranchLeft);
+	this->addElement(cactiStalkLeft);
+	this->addElement(cactiStalkRight);
+
+}
+
+
+

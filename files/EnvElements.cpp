ef8e90b91930de50aa8fd137d1f5d7811403b4b3
michal <mi.sob1238@gmail.com>
2021-01-27 19:15:49 +0100

    Modified the class which keeps the elements of the enviroment to generate the new type of cacti with 2/9 probability

--- files/EnvElements.cpp
+++ files/EnvElements.cpp
@@ -5,20 +5,37 @@ EnvElements::EnvElements(glm::vec3 locomotivePosition) {
 	for (int i = 0; i < 80; i++)
 	{
 		auto a = rand() % 15 + 5;
-		auto b = rand() % 90 ;
+		auto b = rand() % 90;
 		b -= 45;
 		auto c = rand() % 90;
-		cacti.setPosition({ a,0 , locomotivePosition.z - b });
-		cacti.rotate({ 0,c,0 });
-		cvec.emplace_back(cacti);
+		if (c < 70) {
 
+
+			cacti.setPosition({ a,0 , locomotivePosition.z - b });
+			cacti.rotate({ 0,c,0 });
+			cvec.emplace_back(cacti);
+		}
+		else {
+			bigCacti.setPosition({ a,0 , locomotivePosition.z - b });
+			bigCacti.rotate({ 0,c,0 });
+			cvec.emplace_back(bigCacti);
+		}
 		auto a1 = (rand() % 15 + 5) * -1;
-		auto b1 = rand() % 90 ;
+		auto b1 = rand() % 90;
 		b1 -= 45;
 		auto c1 = rand() % 90;
-		cacti.rotate({ 0,c1,0 });
-		cacti.setPosition({ a1,0 ,locomotivePosition.z - b1 });
-		cvec.emplace_back(cacti);
+		if (c1 < 70) {
+
+
+			cacti.setPosition({ a1,0 , locomotivePosition.z - b1 });
+			cacti.rotate({ 0,c1,0 });
+			cvec.emplace_back(cacti);
+		}
+		else {
+			bigCacti.setPosition({ a1,0 , locomotivePosition.z - b1 });
+			bigCacti.rotate({ 0,c1,0 });
+			cvec.emplace_back(bigCacti);
+		}
 	}
 
 }
@@ -44,17 +61,35 @@ void EnvElements::draw(glm::vec3 locomotivePosition) {
 			auto a = rand() % 15 + 5;
 			auto b = rand() % 15 + 30;
 
-			auto c = rand() % 90;
-			cacti.setPosition({ a,0 , locomotivePosition.z - b });
-			cacti.rotate({ 0,c,0 });
-			cvec.emplace_back(cacti);
+			auto c = rand() % 90; 
+			if (c < 70) {
+
+
+				cacti.setPosition({ a,0 , locomotivePosition.z - b });
+				cacti.rotate({ 0,c,0 });
+				cvec.emplace_back(cacti);
+			}
+			else {
+				bigCacti.setPosition({ a,0 , locomotivePosition.z - b });
+				bigCacti.rotate({ 0,c,0 });
+				cvec.emplace_back(bigCacti);
+			}
 
 			auto a1 = (rand() % 15 + 5) * -1;
 			auto b1 = rand() % 15 + 30;
 			auto c1 = rand() % 90;
-			cacti.rotate({ 0,c1,0 });
-			cacti.setPosition({ a1,0 ,locomotivePosition.z - b1 });
-			cvec.emplace_back(cacti);
+			if (c1 < 70) {
+
+
+				cacti.setPosition({ a1,0 , locomotivePosition.z - b1 });
+				cacti.rotate({ 0,c,0 });
+				cvec.emplace_back(cacti);
+			}
+			else {
+				bigCacti.setPosition({ a1,0 , locomotivePosition.z - b1 });
+				bigCacti.rotate({ 0,c,0 });
+				cvec.emplace_back(bigCacti);
+			}
 		}
 		
 		LastLocomotivePosition = locomotivePosition;
@@ -70,16 +105,34 @@ void EnvElements::draw(glm::vec3 locomotivePosition) {
 			auto b = rand() % 15 + 30;
 
 			auto c = rand() % 90;
-			cacti.setPosition({ a,0 , locomotivePosition.z + b });
-			cacti.rotate({ 0,c,0 });
-			cvec.emplace(cvec.begin(),cacti);
-
+			if (c < 70) 	
+			{
+				cacti.setPosition({ a,0 , locomotivePosition.z + b });
+				cacti.rotate({ 0,c,0 });
+				cvec.emplace(cvec.begin(), cacti);
+			}
+			else
+			{
+				bigCacti.setPosition({ a,0 , locomotivePosition.z + b });
+				bigCacti.rotate({ 0,c,0 });
+				cvec.emplace(cvec.begin(), bigCacti);
+			}
 			auto a1 = (rand() % 15 + 5) * -1;
 			auto b1 = rand() % 15 + 30;
 			auto c1 = rand() % 90;
-			cacti.rotate({ 0,c1,0 });
-			cacti.setPosition({ a1,0 ,locomotivePosition.z + b1 });
-			cvec.emplace(cvec.begin(), cacti);
+			if (c1 < 70)
+			{
+				cacti.rotate({ 0,c1,0 });
+				cacti.setPosition({ a1,0 ,locomotivePosition.z + b1 });
+				cvec.emplace(cvec.begin(), cacti);
+			}
+			else 
+			{
+				bigCacti.rotate({ 0,c1,0 });
+				bigCacti.setPosition({ a1,0 ,locomotivePosition.z + b1 });
+				cvec.emplace(cvec.begin(), bigCacti);
+			}
+			
 		}
 
 		LastLocomotivePosition = locomotivePosition;
--- files/EnvElements.h
+++ files/EnvElements.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Cacti.h"
+#include "BigCacti.h"
 class EnvElements
 {
 private:
 	std::vector<Composite> cvec;
 	glm::vec3 LastLocomotivePosition;
 	Cacti cacti = Cacti({ 0,0,0 });
+	BigCacti bigCacti = BigCacti({ 0,0,0 });
 
 public:
 	EnvElements(glm::vec3 locomotivePosition);

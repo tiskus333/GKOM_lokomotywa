4976ab61a3fb186a9d909f3c8bdaeb1b2f750155
Mateusz Chrusciel <tiskus.3009@gmail.com>
2021-01-28 15:45:21 +0100

    new smoke shape

--- files/SmokeParticle.cpp
+++ files/SmokeParticle.cpp
@@ -2,9 +2,17 @@
 
 SmokeParticle::SmokeParticle(const glm::vec3& position, float maxLife): Composite(position), maxLifeTime(maxLife)
 {
-	c2.rotate({ 45,45,45 });
+	//gwiazdki
+	c1.rotate({ 45,0,0 });
+
+	//chmurki
+	/*c1.rotate({ 0,0,90 });
+	c2.rotate({ 0,0,90 });
+	c3.rotate({ 0,0,90 });*/
+
 	addElement(c1);
 	addElement(c2);
+	//addElement(c3);
 }
 
 bool SmokeParticle::update(float deltaTime)
@@ -12,8 +20,8 @@ bool SmokeParticle::update(float deltaTime)
 	lifeTime += deltaTime;
 
 	int rot = rand() % 10;
-	move({ 0,0.04 * (1.f - (lifeTime / maxLifeTime)),0 });
-	rotate({ rot,rot,rot });
+	move({ 0,0.04 * (1.1f - (lifeTime / maxLifeTime)),0 });
+	//rotate({ 0,30,0 });
 	scale(glm::vec3{ 1.01,1.01,1.01 });
 
 	return lifeTime > maxLifeTime;
